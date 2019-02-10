06f82cb6d682bcd9deb02428d40816b84c53a0d2
Henry A. Mendoza Puerta <hmendo81@gmail.com>
2019-02-10 11:00:54 -0500

    CRUD Clientes

---
+++ spring-boot-backend-apirest/src/main/java/com/hampcode/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,17 @@
+package com.hampcode.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.hampcode.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	
+	public List<Cliente> findAll();
+	
+	public void save(Cliente cliente);
+	
+	public Cliente findById(Long id);
+	
+	public void delete(Cliente cliente);
+
+}
